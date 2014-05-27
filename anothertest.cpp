d445ba608ee9cc69f28af2cded2e6d3622384646
baoweiwei <baoweiwei100@gmail.com>
2014-05-27 14:08:02 +0800

    Ben:just test reset

---
+++ anothertest.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: anothertest.cpp
+	> Author: baoweiwei
+	> Mail: baoweiwei100@gmail.com 
+	> Created Time: Tue May 27 14:06:17 2014
+ ************************************************************************/
+include<iostream>
+using namespace std;
+int main(){
+	cout<<"hi "<<endl;
+	return 0;
+}
